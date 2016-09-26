c31ddf493fc2e371d5cb129269516139ed8dcec7
Christopher Burns <christopherburns@Chris-Burns-ZCW-6.local>
2016-09-26 16:46:24 -0400

    initial commit

---
+++ src/main/java/io/zipcoder/pets/Pet.java
@@ -0,0 +1,25 @@
+package io.zipcoder.pets;
+import java.util.*;
+import java.util.Scanner;
+
+public class Pet {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("How many pets do you have?");
+        int numPets = input.nextInt();
+
+        for (int i = 0; numPets )
+        System.out.println("What kind of pet/pets?");
+        String line2 = input.nextLine();
+
+
+        System.out.println("What is/are your pets' name?");
+        String line3 = input.nextLine();
+
+    }
+}
